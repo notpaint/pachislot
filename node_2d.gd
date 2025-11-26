fa4b37d6f3d7ce31bd0c98f267cb609ea695bd7d
notpaint <91761698+notpaint@users.noreply.github.com>
2025-11-27 03:02:53 +0900

    新sql表に対応 ベル以外も引き込むように

--- node_2d.gd
+++ node_2d.gd
@@ -7,6 +7,7 @@ const pattern_per : float = 1.0 / pattern_sum
 var db : SQLite
 var db_path = "database_v2.db"
 
+var weight_table : Dictionary = {}
 var flag_table : Dictionary = {}
 var control_table : Dictionary = {}
 
@@ -17,6 +18,8 @@ var active_tween : Tween
 var current_pixel : float = 0.0
 var current_scale : float = 0.0
 var current_ID : int = 0
+
+var result_flag = null
 
 @onready var L_reel = $window/L_reel
 
@@ -42,95 +45,116 @@ func _unhandled_input(event):
 		if event.keycode == KEY_SPACE:
 			if not is_spinning:
 				var rand_num :int = drawing()
-				var result_role = select_flags(rand_num)
-				if not result_role == "vac": 
-					print(flag_table[result_role]["target"])
+				result_flag = select_flags(rand_num)
+				print(result_flag)
+				if not result_flag == "vac": 
+					pass
 				
 				is_spinning = true
-				print(flag_table)
 
 		if event.keycode == KEY_ENTER:
 			if is_spinning:
-				stop_reels('middleBell')
+				stop_reels(result_flag)
 				is_spinning = false
 
 
 
 #フラグデータ読み込み
 func load_data_from_db():
+	var results
+	var order
 
-	var order = """
-	SELECT
-	f.flag_name, f.state, f.weight,
-	r.name, r.kind , r.payout, r.pattern
-	FROM
-	mapping AS m
-	JOIN
-	roles AS r
-	ON
-	m.role_id = r.id
-	JOIN
-	flags AS f
-	ON
-	m.flag_id = f.id
-	ORDER BY f.id
-	"""
-
-	db.query(order)
-	var results = db.query_result
-	for row in results:
-		var f_name = row["flag_name"]
-		if not flag_table.has(f_name):
-			flag_table[f_name] = {
-				"weight": row["weight"],
-				"target": []
-			}
-
-		var target_info = {
-			"role_name": row["name"],
-			"kind": row["kind"],
-			"payout": row["payout"],
-			"pattern": row["pattern"]
-		}
-
-		flag_table[f_name]["target"].append(target_info)
-
-		
-
+	#weight_table(フラグの確率表)作成
 	order = """
-	SELECT
-	r.name,
-	s.reel_pos, s.reel_ID, s.slide
+	SELECT 
+	f.flag,
+	ws.weight_state,
+	ft.weight
 	FROM
-	slides AS s
+	flags AS f
 	JOIN
-	roles AS r
-	ON
-	s.role_ID = r.id
+	flag_table AS ft ON ft.flag_id = f.id
+	JOIN
+	weight_status AS ws ON ft.weight_status_id = ws.id
 	"""
 
 	db.query(order)
 	results = db.query_result
-	for row in results:
-		var r_name = row["name"]
-		var r_pos = int(row["reel_pos"])
-		var r_ID = int(row["reel_ID"])
-		var r_slide = int(row["slide"])
-		if not control_table.has(r_name):
-			control_table[r_name] = [[], [], []]
-			for i in range(3):
-				control_table[r_name][i].resize(pattern_sum)
-				control_table[r_name][i].fill(0)
-		control_table[r_name][r_pos][r_ID] = r_slide
+
+	for item in results:
+		var flag = item["flag"]
+		var weight_state = item["weight_state"]
+		var weight = int(item["weight"])
+		if not weight_table.has(weight_state):
+			weight_table[weight_state] = []
+		var data = {"flag": flag,"weight" : weight}
+		weight_table[weight_state].append(data)
+
+
+	#flag_table(フラグの重複役一覧)作成
+	order = """
+	SELECT
+	f.flag,
+	r.role, r.kind, r.payout, r.pattern
+	FROM
+	flag_role_map AS fr
+	JOIN
+	flags AS f ON fr.flag_ID = f.id
+	JOIN
+	roles AS r ON fr.role_ID = r.id
+	"""
+
+	db.query(order)
+	results = db.query_result
+
+	for item in results:
+		var flag = item["flag"]
+		var role = item["role"]
+		var kind = item["kind"]
+		var payout = int(item["payout"])
+		if not flag_table.has(flag):
+			flag_table[flag] = []
+		var data = {"role": role, "payout":payout, "kind": kind}
+		flag_table[flag].append(data)
+
+
+	#control_table(小役の制御表)作成
+	order = """
+	SELECT
+	r.role,
+	s.reel_pos, s.reel_ID, s.slide
+	FROM
+	slide_table AS s
+	JOIN
+	roles AS r ON s.role_ID = r.id
+	"""
+
+	db.query(order)
+	results = db.query_result
+
+	for item in results:
+		var role = item["role"]
+		var reel_pos = int(item["reel_pos"])
+		var reel_ID = int(item["reel_ID"])
+		var slide = int(item["slide"])
+		if not control_table.has(role):
+			control_table[role] = [[],[],[]]
+			for i in range (3):
+				control_table[role][i].resize(pattern_sum)
+		control_table[role][reel_pos][reel_ID] = slide
+
+	print(weight_table)
+		
 
 
 #フラグ抽選
 func select_flags(value):
-	for flag_name in flag_table:
-		var weight: int = flag_table[flag_name]["weight"]
+	var current_weight_table = weight_table["Normal"]
+	for data in current_weight_table:
+		var weight: int = data["weight"]
 		value -= weight
 		if value < 0:
-			return(flag_name)
+			return(data["flag"])
 	return("vac")
 
 
@@ -149,15 +173,19 @@ func stop_reels(flag):
 	if active_tween:
 		active_tween.kill()
 	
+	if flag == "vac":
+		return
+	
+	var role = flag_table[flag][0]["role"]
 	current_pixel = L_reel.position.y
 	current_scale = fmod((current_pixel / pattern_scale) , 1.0)
 
-	if current_scale < 0.35:
-		return
+	# if current_scale < 0.35:
+	# 	return
 	
 	var target_pixel : float = ceil(current_pixel / pattern_scale) * pattern_scale
 	current_ID = int(floor(fmod((target_pixel /pattern_scale), pattern_sum)))
-	var slide = control_table[flag][0][current_ID]
+	var slide = control_table[role][0][current_ID]
 	target_pixel += (pattern_scale * slide)
 	var target_speed : float = abs(target_pixel - current_pixel) / spin_speed
 
@@ -170,3 +198,4 @@ func stop_reels(flag):
 
 	if active_tween:
 		active_tween.kill()
+	print(current_ID)
