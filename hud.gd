d10716df6cc79ca9a325285c2ae5b0a4456b3986
notpaint <91761698+notpaint@users.noreply.github.com>
2025-12-18 19:07:29 +0900

    スイカの画像変更

--- hud.gd
+++ hud.gd
@@ -1,10 +1,26 @@
 extends CanvasLayer
 
-@onready var main_rom = $"../mainROM"
+@onready var mainROM = $"../mainROM"
 
 @onready var flag_name = $flag_name
 
-func _process(delta):
-	if main_rom:
-		if not str(main_rom.result_flag) == null:
-			flag_name.text = str(main_rom.result_flag)
+# func _process(delta):
+# 	if main_rom:
+		# if not str(main_rom.result_flag) == null:
+		# 	flag_name.text = str(main_rom.result_flag)
+		# if not str(main_rom.current_reel) == null:
+		# 	flag_name.text = str(main_rom.current_reel)
+
+func _ready():
+	if mainROM:
+		mainROM.prized.connect(_on_prized)
+
+func _on_prized(reel_result):
+	print(reel_result)
+
+
+
+func _unhandled_input(event):
+	if event.is_action_pressed("debug"):
+		var font : Font = flag_name.get_theme_font("font_size")
+		print(font)
